e4b22e28d4c92660099675d717ca748a7cc9e95e
Webners1 <Webners1>
2021-10-16 16:08:10 +0500

    Updates

---
+++ precache-manifest.69b8fb9eca42f54ed7052b46edfe94bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db422cf84795a5df33e93dc95d7b96d",
+    "url": "/Web3.0-DROPBOX-clone/index.html"
+  },
+  {
+    "revision": "2f1f27174ac451109a7d",
+    "url": "/Web3.0-DROPBOX-clone/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "7dbc89e8ccfa19b2e513",
+    "url": "/Web3.0-DROPBOX-clone/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "2f1f27174ac451109a7d",
+    "url": "/Web3.0-DROPBOX-clone/static/js/2.3d5fb802.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/Web3.0-DROPBOX-clone/static/js/2.3d5fb802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbc89e8ccfa19b2e513",
+    "url": "/Web3.0-DROPBOX-clone/static/js/main.41e1bf42.chunk.js"
+  },
+  {
+    "revision": "c60e415de4c61bceec7d",
+    "url": "/Web3.0-DROPBOX-clone/static/js/runtime-main.d28eb89e.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/Web3.0-DROPBOX-clone/static/media/box.df8b84d5.png"
+  }
+]);
